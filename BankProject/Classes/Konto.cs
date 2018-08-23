d61090e91cbba4dec6d7fe1847b93ef4425d2f4d
MuhammedSarac <muhammedsarac@live.dk>
2018-08-23 11:23:11 +0200

    Added Kunde Konto object

---
+++ BankProject/Classes/Konto.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankProject.Classes
+{
+    class Konto
+    {
+        private string _KontoName;
+        private int _KontoNr;
+        private int _Saldo;
+        private int _Rente;
+        private int _CreateDato;
+
+
+        public string KontoName
+        {
+            set { _KontoName = value; }
+            get { return _KontoName; }
+        }
+
+
+
+        public int Saldo
+        {
+            set { _Saldo = value; }
+            get { return _Saldo; }
+        }
+
+        public int Rente
+        {
+            set { _Rente = value; }
+            get { return _Rente; }
+        }
+
+        public int CreateDato
+        {
+            get => _CreateDato;
+            set => _CreateDato = value;
+        }
+        public int KontoNr
+
+        {
+            get => _KontoNr;
+            set => _KontoNr = value;
+        }
+
+        public Konto(string kontoname, int kontonr, int saldo, int rente, int dato)
+        {
+            KontoName = kontoname;
+            KontoNr = kontonr;
+            Saldo = saldo;
+            Rente = rente;
+            CreateDato = dato;
+        }
+        public override string ToString()
+        {
+            return string.Format($"Konto Navn: {KontoName} Kontonr: {KontoNr} Saldo: {Saldo} Rente: {Rente} OprettelsesDato {CreateDato}");
+        }
+    }
+}
--- BankProject/Classes/Kunde.cs
+++ BankProject/Classes/Kunde.cs
@@ -8,5 +8,50 @@ namespace BankProject.Classes
 {
     class Kunde
     {
+        class Kunde
+        {
+            private int _Nr;
+            private string _Name;
+            private string _LastName;
+            private string _Adresse;
+            private int _Telephone;
+            private int _CreateDato;
+            public List<Konto> Konto = new List<Konto>();
+
+
+            public string Name { get => _Name; set => _Name = value; }
+            public string LastName { get => _LastName; set => _LastName = value; }
+            public string Adresse { get => _Adresse; set => _Adresse = value; }
+            public int Nr { get => _Nr; set => _Nr = value; }
+            public int Telephone { get => _Telephone; set => _Telephone = value; }
+            public int CreateDato { get => _CreateDato; set => _CreateDato = value; }
+
+
+            public Kunde(int nr, string name, string lastName, string adresse, int telephone, int createDato)
+            {
+                Nr = nr;
+                Name = name;
+                LastName = lastName;
+                Adresse = adresse;
+                Telephone = telephone;
+                CreateDato = createDato;
+            }
+
+            public void AddKonto(Konto a)
+            {
+                Konto.Add(a);
+            }
+
+            public override string ToString()
+            {
+                string s = "";
+                foreach (var item in Konto)
+                {
+                    s = s + " " + item + Environment.NewLine;
+                }
+                return string.Format($"Kundenr: {Nr} Name: {Name} LastName: {LastName} Adresse: {Adresse} Telephone: {Telephone} CreateDato: {CreateDato}\r\nAdresser:\r\n{s}");
+            }
+
+        }
     }
 }
