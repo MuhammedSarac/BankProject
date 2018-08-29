40780fe39e8e69bfac70510542586857e7a0a39b
Ali Saraç <alisarac@live.com>
2018-08-29 10:26:55 +0200

    Added comment

--- BankProject/Classes/DBConnection.cs
+++ BankProject/Classes/DBConnection.cs
@@ -1,13 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace BankProject.Classes
 {
-    public class DBConnection
+    public class DbConnection
     {
+        //DB Connection
+        public static string DbAdress = @"Data Source=" + Environment.CurrentDirectory + "\\DB\\Bank.db;Version=3;New=False;Compress=True,Read Only= False;";
+        public static string ConStatus;
 
+        public static void ConTest()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(DbAdress))
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    try
+                    {
+                        conn.Open();
+                        ConStatus = "Connected to DataBase..";
+                    }
+                    catch (Exception)
+                    {
 
-        public static string DBAdress = @"Data Source=" + Environment.CurrentDirectory +
-                                        "\\DB\\Bank.db;Version=3;New=False;Compress=True;Read Only;False;";
+                        ConStatus = "Connection Faild..";
+                    }
+                }
+                else
+                {
+                    ConStatus = "Connected to DataBase..";
+                }
+            }
+        }
 
     }
 }
